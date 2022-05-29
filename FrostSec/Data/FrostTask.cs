0c7f17b28e32c4df1287fe08015d4042ba6ac8a3
Christian Enamorado <cenamorado3@gmail.com>
2022-05-28 23:20:22 -0500

    this is a terrible commit message

---
+++ FrostSec/Data/FrostTask.cs
@@ -0,0 +1,26 @@
+﻿namespace FrostSec.Data
+{
+    public class FrostTask
+    {
+
+        public string Name { get; set; }
+        public List<string> States = new() { "ToDo", "Started", "Completed" };
+        public string State { get; set; } = "ToDo";
+        public string Style { get; set; } = "";
+        public double X { get; set; }
+        public double Y { get; set; }
+
+        public void AdvanceState()
+        {
+            int nextStateIndex = States.IndexOf(State) + 1;
+            if (nextStateIndex < States.Count)
+            {
+                string nextState = States[nextStateIndex];
+                State = nextState;
+                //X = 0;
+                //Y = 0;
+                Style = "";
+            }
+        }
+    }
+}
